b6e53fce1f921adc5f51b6f04f77b0bec6ae9528
Josh Hall <joshhall1991@gmail.com>
2021-04-23 23:44:33 -0500

    connected front and back end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,22 @@
+import React from "react"
 import './App.css';
 import LoginPage from './component/LoginPage';
 import SlipsPage from './component/SlipsPage';
 
 function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div>
       {/* <LoginPage></LoginPage> */}
+      <p>{!data ? "Loading..." : data}</p>
       <SlipsPage></SlipsPage>
     </div>
   )}
