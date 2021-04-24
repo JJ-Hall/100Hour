c6ed355ccf46677613ee68b07fc2dffa6eb4ab0c
Josh Hall <joshhall1991@gmail.com>
2021-04-24 00:17:46 -0500

    1

--- back-end/server.js
+++ back-end/server.js
@@ -6,6 +6,10 @@ app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
 
+  app.get("/users", (req, res) => {
+    res.json({ message: "This is the user" });
+  });
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
